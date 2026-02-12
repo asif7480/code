bb178def4831fcf897aea1105fbdc410bc2321be
a.a.khan7480@gmail.com <aziaptech@gmail.com>
2026-02-12 10:34:33 +0500

    FEAT: added edit module

--- src/app/pages/edit-product/edit-product.ts
+++ src/app/pages/edit-product/edit-product.ts
@@ -2,29 +2,40 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Product } from '../../models/product';
 import { ProductService } from '../../services/product.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-edit-product',
-  imports: [FormsModule],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './edit-product.html',
   styleUrl: './edit-product.css',
 })
 export class EditProduct {
+  productObj: Product = {
+    productName: '',
+    description: '',
+    price: 0,
+  };
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly productService: ProductService,
+    private readonly router: Router,
+  ) {}
 
-    
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id')!;
+    console.log(id);
+    this.productService.getSingleProduct(id).subscribe((response) => {
+      this.productObj = response as Product;
+    });
+  }
 
-    productObj: Product = {
-      productName: "",
-      description: "",
-      price: 0
-    }
-    constructor(private readonly productService: ProductService){
-
-    }
-
-
-
-    handleSubmit(){
-      
-    }
+  handleSubmit(id: string) {
+    this.productService.updateProduct(id, this.productObj).subscribe((response) => {
+      alert(`Product has been updated.`);
+      this.router.navigate(['']);
+    });
+  }
 }
